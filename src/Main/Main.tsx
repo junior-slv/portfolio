9b9192befca14da855344f9f849b5148fe31d382
Junior <108402999+junior-slv@users.noreply.github.com>
2022-11-05 13:24:08 -0500

    Add files via upload

---
+++ src/Main/Main.tsx
@@ -0,0 +1,10 @@
+import { StyledButton } from './index'
+function Main(){
+    return ( 
+        <div className="container">
+            <StyledButton>BOTAO</StyledButton>
+        </div>
+     );
+}
+ 
+export default Main;
---
+++ src/Main/index.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+export const StyledButton = styled.button`
+  background-color: black;
+  font-size: 32px;
+  color: white;
+`;
